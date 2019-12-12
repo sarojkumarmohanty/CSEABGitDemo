9a0a6d84dfa8de21f15d1587cf116280549537d1
Saroj Mohanty <Saroj Mohanty@SarojMohanty-PC>
2019-12-12 11:59:19 +0530

    changed commit

--- src/com/trident/service/UserServiceImpl.java
+++ src/com/trident/service/UserServiceImpl.java
@@ -4,8 +4,7 @@ public class UserServiceImpl implements UserServer {
 
 	@Override
 	public int add(int a, int b) {
-		//please write your own version of add method
-		return 0;
+		return a+b;
 	}
 
 }
